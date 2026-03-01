904cbc017b9fabf0dc046773c3a3fe5c5e461492
Prikshit <Prikshitrao813@gmail.com>
2026-03-02 03:34:04 +0530

    Created a JS file named app

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hello world") 
+var name= "prikshit" //functiom scoped,hoisted -avoid in modern js 
+let age= 20;//block scoped, can be reassigned, 
+const PI= 3.14159;//blocked scoped, cannot be reassigned
+let score= 0;
+score = 10;//ok- reassignment allowed for let
+const MAX_SCORE= 100;   
+// MAX_SCORE = 200; //typeerror- assignment to constant variable
+    
